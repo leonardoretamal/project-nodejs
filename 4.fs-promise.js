bb92ba891f36033a950992b3718cc23f3d273635
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-10-16 15:15:16 -0300

    Agregar implementación de lectura de archivos asíncrona en fs-promise.js

---
+++ 4.fs-promise.js
@@ -0,0 +1,18 @@
+// esto solo en los modulos nativos que no tienen promesas nativas.
+//const { promisify } = require('node:util')
+//const readFilePromise = promisify(fs.readFile)
+
+const fs = require('node:fs/promises')
+
+
+console.log('Leyendo el primer archivo...')
+fs.readFile('./archivo.txt', 'utf-8').then(text => {
+    console.log('Primer texto: ', text)
+}) //forma asíncrona con promesas
+
+console.log('hacer cosas mientras lee el archivo...')
+
+console.log('Leyendo el segundo archivo...')
+fs.readFile('./archivo2.txt', 'utf-8').then(text => {
+    console.log('Segundo texto: ', text)
+})
