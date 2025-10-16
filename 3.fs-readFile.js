29d5fbea39d1f0373c32eac3f14bede9a26e2d3d
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-10-16 15:04:23 -0300

    Agregar implementación de lectura de archivos y actualizar contenido de archivos de texto async

---
+++ 3.fs-readFile.js
@@ -0,0 +1,14 @@
+const fs = require('node:fs')
+
+
+console.log('Leyendo el primer archivo...')
+fs.readFile('./archivo.txt', 'utf-8', (err, text) => { //ejecutas este callback cuando termine 
+    console.log('Primer texto: ',text)
+}) //forma asíncrona con callback
+
+console.log('hacer cosas mientras lee el archivo...')
+
+console.log('Leyendo el segundo archivo...')
+fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
+    console.log('Segundo texto: ',text)
+})
