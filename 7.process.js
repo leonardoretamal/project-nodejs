8f272af22f9559758e1b9201680dfc8a8d3703d3
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-10-20 18:51:22 -0300

    creacion de archivo process

---
+++ 7.process.js
@@ -0,0 +1,17 @@
+//argumentos de entrada
+
+//console.log(process.argv) //recupera argumentos de entrada.
+
+//controlar proceso y su salida
+
+//process.exit(1)
+
+//podemos controlar los eventos del proceso
+//process.on('exit', () => {
+    //limpiar los recursos
+//})
+// current working directory
+console.log(process.cwd());
+
+//platform 
+console.log(process.env.PEPITO)
