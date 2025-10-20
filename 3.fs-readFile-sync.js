a4de430c396a1fca2e3c8b777fbf3944a87c765d
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-10-20 18:22:47 -0300

    Agregar implementación de lectura de archivos de forma síncrona en fs-readFile-sync.js

---
+++ 3.fs-readFile-sync.js
@@ -0,0 +1,12 @@
+const fs = require('node:fs')
+
+
+console.log('Leyendo el primer archivo...')
+const text = fs.readFileSync('./archivo.txt', 'utf-8')
+console.log('Primer texto: ',text)
+
+console.log('hacer cosas mientras lee el archivo...')
+
+console.log('Leyendo el segundo archivo...')
+const text2 = fs.readFileSync('./archivo2.txt', 'utf-8')
+console.log('Segundo texto: ', text2)
