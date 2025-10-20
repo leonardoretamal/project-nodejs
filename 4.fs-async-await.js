5b314534f10ffc82c0a9b8c83ce105f27e1e0607
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-10-20 18:13:50 -0300

    Agregar implementación de lectura de archivos utilizando async/await en fs-async-await.js y fs-async-await-parallel.mjs

---
+++ 4.fs-async-await.js
@@ -0,0 +1,17 @@
+const { readFile } = require('node:fs/promises');
+
+//IIFE - Immediately Invoked Function Expression
+
+(
+    async () => {
+        console.log('Leyendo el primer archivo...')
+        const text = await readFile('./archivo.txt', 'utf-8')
+        console.log('Primer archivo: ', text)
+
+        console.log('hacer cosas mientras lee el archivo...')
+
+        console.log('Leyendo el segundo archivo...')
+        const text2 = await readFile('./archivo2.txt', 'utf-8')
+        console.log('Segundo texto: ', text2)
+    }
+)()
