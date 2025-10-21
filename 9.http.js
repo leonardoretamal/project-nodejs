c94c0c010ca2bb7f1a6778e0888de0663ccf615b
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-10-20 23:39:45 -0300

    cambios menores

--- 9.http.js
+++ 9.http.js
@@ -1,13 +1,15 @@
 const http = require('node:http')
 const { findAvailablePort } = require('./10.free-port')
 
-const port = process.env.PORT ?? 3000
+const desiredPort = process.env.PORT ?? 3000
 
 const server = http.createServer((req, res) => {
     console.log('request received')
     res.end('Hola mundo.')
 })
 
-findAvailablePort(1234).then(port => {
-    console.log(`server listening on port http://localhost:${port}`)
+findAvailablePort(desiredPort).then(port => {
+    server.listen(port, () => {
+        console.log(`server listening on port http://localhost:${port}`)
+    })
 })
